72a0affdc387c11d7941b1c89dd96364b3209aba
Twarug <patryk280502@gmail.com>
2024-12-13 17:11:55 +0100

    Update dependencies & gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version("1.9.20")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.shadow)
+    id("org.jetbrains.kotlin.plugin.serialization") version (libs.versions.kotlin)
 }
 
 group = "dev.remodded"
@@ -16,19 +16,14 @@ repositories {
 }
 
 dependencies {
-    // Velocity
-    compileOnly("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+    compileOnly(libs.velocity.api)
 
-    // Kotlin
+    implementation(libs.kyori.pagginate)
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // Kyorinet
-    implementation("net.kyori:adventure-text-feature-pagination:4.0.0-SNAPSHOT")
 }
 
 tasks {
-    val javaVersion = JavaVersion.VERSION_11
-
     shadowJar {
         archiveBaseName.set("ReWhitelist")
         archiveClassifier.set("")
@@ -38,14 +33,7 @@ tasks {
         dependsOn(shadowJar)
     }
 
-    compileKotlin {
-        kotlinOptions { jvmTarget = javaVersion.toString() }
-    }
-
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
     }
 }
