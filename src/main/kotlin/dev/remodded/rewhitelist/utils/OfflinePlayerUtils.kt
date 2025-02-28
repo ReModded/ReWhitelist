d36942a54eb1f6bdf2a91eea4b60a2028df3726e
Twarug <patryk@twardosz.dev>
2025-02-28 12:19:00 +0100

    Add eager return for online players

--- src/main/kotlin/dev/remodded/rewhitelist/utils/OfflinePlayerUtils.kt
+++ src/main/kotlin/dev/remodded/rewhitelist/utils/OfflinePlayerUtils.kt
@@ -1,25 +1,40 @@
 package dev.remodded.rewhitelist.utils
 
 import com.velocitypowered.api.util.UuidUtils
+import dev.remodded.rewhitelist.ReWhitelist
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URI
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 object OfflinePlayerUtils {
     fun getOfflinePlayerUUID(username: String): UUID? {
-        try {
+        val onlinePlayer = ReWhitelist.server.getPlayer(username).getOrNull()
+
+        if (onlinePlayer != null)
+            return onlinePlayer.uniqueId
+
+        val stringUUID = try {
             val url = URI("https://api.mojang.com/users/profiles/minecraft/$username").toURL()
             val urlConnection = url.openConnection()
             BufferedReader(InputStreamReader(urlConnection.getInputStream())).use { reader ->
                 val json = reader.readText()
+
                 val index = json.indexOf("\"id\" : \"") + 8
-                val string = json.substring(index, json.indexOf("\"", index))
-                return UuidUtils.fromUndashed(string)
+                if (index < 0)
+                    return null
+
+                val endIndex = json.indexOf("\"", index)
+                if (endIndex < 0)
+                    return null
+
+                json.substring(index, endIndex)
             }
-        } catch (ex: Exception) {
-            ex.printStackTrace()
+        } catch (_: Exception) {
+            return null
         }
-        return null
+
+        return UuidUtils.fromUndashed(stringUUID)
     }
 }
